4b1ea026895cd6077804fb5b850616898dc006e7
LucasLara99 <lucaslara99@gmail.com>
2024-04-16 19:42:18 +0200

    Cors configuration and database connected

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.beans.factory.annotation.Value;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
