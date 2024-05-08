87832cb63d937148f2b9deb0d9582bb3bb1f1784
Lucas Lara <lucaslara99@gmail.com>
2024-05-08 20:27:27 +0200

    Register method response improved

--- src/main/java/com/example/demo/controllers/RegistrationController.java
+++ src/main/java/com/example/demo/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.requests.RegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +28,7 @@ public class RegistrationController {
     public ResponseEntity<?> register(@RequestBody RegistrationRequest registrationRequest) {
         // Validar los datos del usuario
         if (userRepository.findByEmail(registrationRequest.getEmail()) != null) {
-            return ResponseEntity.badRequest().body(Map.of("error", "El email ya está registrado"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "El email ya está registrado"));
         }
 
         // Hashear la contraseña
@@ -41,6 +42,6 @@ public class RegistrationController {
         // Guardar el objeto User en la base de datos
         userRepository.save(user);
 
-        return ResponseEntity.ok().body(Map.of("message", "Registro exitoso"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "Registro exitoso"));
     }
 }
