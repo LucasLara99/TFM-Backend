036be9f60eacc19f672dceb68f523d9abd1b1133
Lucas Lara <lucaslara99@gmail.com>
2024-05-14 23:51:03 +0200

    Http status implemented in league controller

--- src/main/java/com/example/demo/controllers/LeagueController.java
+++ src/main/java/com/example/demo/controllers/LeagueController.java
@@ -2,9 +2,12 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.League;
 import com.example.demo.services.LeagueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -18,7 +21,12 @@ public class LeagueController {
     }
 
     @GetMapping("/all")
-    public List<League> getAllLeagues() {
-        return leagueService.getAllLeagues();
+    public ResponseEntity<?> getAllLeagues() {
+        try {
+            List<League> leagues = leagueService.getAllLeagues();
+            return new ResponseEntity<>(leagues, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error retrieving leagues: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
