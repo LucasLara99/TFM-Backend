4f35b1096574725bdb813a41c25faa3b11326567
Lucas Lara <lucaslara99@gmail.com>
2024-05-16 18:29:31 +0200

    Bug fixed

--- src/main/java/com/example/demo/controllers/JwtUtil.java
+++ src/main/java/com/example/demo/controllers/JwtUtil.java
@@ -2,11 +2,8 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.User;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
@@ -16,12 +13,10 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private SecretKey secretKey;
-    private Long expirationTime;
+    private final Long expirationTime;
 
     public JwtUtil() {
         // Generate a secure key
-        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
         this.expirationTime = 3600000L; // Set your desired expiration time here
     }
 
@@ -30,11 +25,10 @@ public class JwtUtil {
                 .setSubject(user.getEmail())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(secretKey)
                 .compact();
     }
 
-    public Map<String, String> parseJwtToken(String token) {
+    public void parseJwtToken(String token) {
         String[] parts = token.split("\\."); // Divide el token en partes
         Map<String, String> parsedToken = new HashMap<>();
         if (parts.length == 3) { // Verifica si el token tiene tres partes
@@ -44,6 +38,5 @@ public class JwtUtil {
             parsedToken.put("header", header);
             parsedToken.put("payload", payload);
         }
-        return parsedToken;
     }
 }
